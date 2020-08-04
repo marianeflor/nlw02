66521f9604846cd6081d1b79b1a5f3e387656c35
Mariane Flor <marianeflor@gmail.com>
2020-08-04 19:15:52 -0300

    dia 2 - server - rotas e db com sqlite e knex

---
+++ server/src/database/migrations/03_create_connections.ts
@@ -0,0 +1,23 @@
+import Knex from 'knex'
+
+export async function up(knex: Knex){
+  return knex.schema.createTable('connections', table => {
+    table.increments('id').primary()
+
+    //relacionando com usuario
+    table.integer('user_id')
+        .nullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+
+    table.timestamp('created_at')
+        .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+        .notNullable()
+  })
+}
+
+export async function down(knex: Knex){
+  return knex.schema.dropTable('connections')
+}
---
+++ server/src/utils/convertHourToMinutes.ts
@@ -0,0 +1,9 @@
+const convertHousToMinutes = (time:string) => {
+
+    const [hour, minutes] = time.split(':').map(Number)
+    const timeInMinutes = (hour * 60) + minutes
+
+    return timeInMinutes
+}
+
+export default convertHousToMinutes
