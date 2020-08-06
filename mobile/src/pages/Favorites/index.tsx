c721dd6db38c3950d1104846bae539b5df5d45c3
Mariane Flor <marianeflor@gmail.com>
2020-08-06 18:57:32 -0300

    dia 4 - mobile - landing, pageheader, giveclasses, rotas

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import PageHeader from '../../components/PageHeader'
+
+import styles from './styles'
+
+const Favorites = () => {
+  return (
+    <View style={styles.container}>
+      <PageHeader title='Meus proffys favoritos'/>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default Favorites
